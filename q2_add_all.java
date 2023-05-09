54f41551b001c7b6320d171b3414a570b4433744
soubhagya180 <131396761+soubhagya180@users.noreply.github.com>
2023-05-09 16:47:01 +0530

    Add files via upload

---
+++ q2_add_all.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class q2_add_all{
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a:- ");
+    int a=sc.nextInt();
+    int i=1;
+    while (i<=a){
+        System.out.print(i+" ");
+        i++;
+    }
+}
+}
